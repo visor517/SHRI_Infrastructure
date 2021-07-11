5431124a1b34d19f36504d8e8d9db2feabcea9d3
visor517 <visor517@yandex.ru>
2021-07-11 19:10:59 +0300

    added /notify-build-result

---
+++ server/actions/getResult.js
@@ -0,0 +1,18 @@
+const todoLog = require("./todoLog")
+const results = require('../index')
+
+module.exports = (req,res) => {
+    const params = req.query
+    if (params && params.id && params.status && params.log) {
+        results.push(params)
+        todoLog(`Получен результат сборки ${params.id}: ${params.status}`)
+        res.json({ error: 0})
+    }
+    else {
+        todoLog(`Неудачная попытка получения результатов сборки ${params.id}, статус ${params.status}, лог ${params.log}`)
+        res.json({ error: 1})
+    }  
+}
+
+// нет проверки на правильность заполнения и повторки
+// и запускалась ли вообще эта сборка
--- server/index.js
+++ server/index.js
@@ -1,21 +1,20 @@
-let agents = []
+let agents = []     // список зарегистрированных агентов
+let results = []    // список полученных результатов
 module.exports = agents
+module.exports = results
 
 const express = require('express')
 const {port} = require('./server-conf.json')
 const regAgent = require('./actions/regAgent')
+const getResult = require('./actions/getResult')
 
 const app = express()
 
 app.get('/notify-agent', regAgent)
+app.get('/notify-build-result', getResult)
 
-app.get('/agents-list', (req,res) => {
+app.get('/agents-list', (req,res) => {  // для себя проверять список агентов
     res.json(agents)
-})
-
-app.get('/notify-build-result', (req,res) => {
-    console.log(req.query)
-    res.json({error: 0})
 })
 
 app.listen(port, () => console.log(`Server has been started on port ${port} ...`))
